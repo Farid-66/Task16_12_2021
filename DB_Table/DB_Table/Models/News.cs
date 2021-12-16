839678ba0bb803a3a990e1c5283eb060a63b7121
Farid-66 <feridquba66@gmail.com>
2021-12-16 12:22:36 -0800

    Task

---
+++ DB_Table/DB_Table/Models/News.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DB_Table.Models
+{
+    public class News
+    {
+        [Key]
+        public int ID { get; set; }
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [MaxLength(250)]
+        public string Img { get; set; }
+        [Column(TypeName ="date")]
+        public int Review { get; set; }
+        [Column(TypeName ="bit")]
+        public int Like { get; set; }
+        [Column(TypeName = "bit")]
+        public int DisLike { get; set; }
+        public int ReviewCount { get; set; }
+    }
+}
